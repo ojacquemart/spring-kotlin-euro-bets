ad0671ece6a7c4a0f54ae9882cfea3e3802b600a
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-05-30 23:22:45 +0200

    chore: add dummy android.content.Context class
    Spring app context will just start fine, for now

---
+++ src/main/kotlin/android/content/Context.java
@@ -0,0 +1,6 @@
+package android.content;
+
+// @see https://groups.google.com/forum/#!msg/firebase-talk/XLbpLpqCdDI/ncuw32Xr-4YJ
+// dummy class in the right package to make firebase happy
+public class Context {
+}
