86cd4b228370290ee4ef485cd5f4407cb59480ad
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-06-20 08:22:05 +0200

    refactor: rework groups update
    Reduce schedule time to 15 min
    Fix possible race condition issue when many updates at the same time

--- src/main/kotlin/org/ojacquemart/eurobets/firebase/management/group/GroupsUpdateScheduler.kt
+++ src/main/kotlin/org/ojacquemart/eurobets/firebase/management/group/GroupsUpdateScheduler.kt
@@ -5,11 +5,9 @@ import com.firebase.client.FirebaseError
 import com.firebase.client.ValueEventListener
 import org.ojacquemart.eurobets.firebase.Collections
 import org.ojacquemart.eurobets.firebase.config.FirebaseRef
-import org.ojacquemart.eurobets.firebase.rx.RxFirebase
 import org.ojacquemart.eurobets.lang.loggerFor
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.stereotype.Component
-import rx.Observable
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.*
@@ -25,13 +23,17 @@ class GroupsUpdateScheduler(val taskScheduler: TaskScheduler, val groupsUpdater:
         log.debug("Check if groups updated is needed")
 
         if (!updateScheduled) {
+            updateScheduled = true
+
             ref.firebase.child(Collections.settings).child("dayId")
                     .addListenerForSingleValueEvent(object : ValueEventListener {
                         override fun onCancelled(p0: FirebaseError?) {
+                            updateScheduled = false
                         }
 
                         override fun onDataChange(p0: DataSnapshot?) {
                             doUpdateIfDayInGroupPhase(p0?.getValue(Int::class.java))
+                            updateScheduled = false
                         }
                     })
         }
@@ -51,14 +53,12 @@ class GroupsUpdateScheduler(val taskScheduler: TaskScheduler, val groupsUpdater:
     private fun isDayInGroupPhase(dayId: Int) = dayId <= MAX_DAY_ID_GROUP_PHASE
 
     private fun doScheduleUpdate() {
-        updateScheduled = true
         val updateDate = getUpdateDate()
 
         log.info("Schedule groups update in $UPDATE_DELAY_IN_MINUTES minutes et $updateDate")
 
         taskScheduler.schedule({
             groupsUpdater.update()
-            updateScheduled = false
         }, updateDate)
     }
 
@@ -69,14 +69,8 @@ class GroupsUpdateScheduler(val taskScheduler: TaskScheduler, val groupsUpdater:
         return Date.from(ldtInDelay.atZone(ZoneId.systemDefault()).toInstant())
     }
 
-    private fun getDayId(): Observable<RxFirebase.FirebaseChildEvent>? {
-        val refSettings = ref.firebase.child(Collections.settings).child("dayId")
-
-        return RxFirebase.observeChildren(refSettings)
-    }
-
     companion object {
-        val UPDATE_DELAY_IN_MINUTES = 30L
+        val UPDATE_DELAY_IN_MINUTES = 15L
         val MAX_DAY_ID_GROUP_PHASE = 3
     }
 
