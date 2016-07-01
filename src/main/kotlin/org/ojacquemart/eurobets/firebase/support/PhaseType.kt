4f492adff7ea052f22b149fb1124d375f185e68d
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-07-01 10:21:03 +0200

    fix: change phase type mapping
    There was a mismatch between the fixtures stored & the phase scale

--- src/main/kotlin/org/ojacquemart/eurobets/firebase/support/PhaseType.kt
+++ src/main/kotlin/org/ojacquemart/eurobets/firebase/support/PhaseType.kt
@@ -3,7 +3,7 @@ package org.ojacquemart.eurobets.firebase.support
 enum class PhaseType constructor(val state: String) {
     GROUP("group"),
     ROUND_16("round16"),
-    QUARTER("quarter"),
+    QUARTER("quarterfinal"),
     SEMI("semifinal"),
     FINAL("final")
 }
